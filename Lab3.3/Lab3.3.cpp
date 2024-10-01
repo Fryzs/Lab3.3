5055ef285567864acc4997c514a87788fde19dbd
Fryzs <frizoles@gmail.com>
2024-10-01 15:57:19 +0300

    Добавьте файлы проекта.

---
+++ Lab3.3/Lab3.3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string Text;
+    ifstream file("text.txt");
+    getline(file, Text);
+    std::cout << Text << endl;
+
+
+    Text[0] = toupper(Text[0]);
+
+    for (size_t i = 0; i < Text.length(); ++i) {
+        if (Text[i] == ',' || Text[i] == '.') {
+
+            if (i > 0 && Text[i - 1] == ' ') {
+                Text.erase(i - 1, 1);
+                --i; 
+            }
+
+            if (i + 1 < Text.length() && Text[i + 1] != ' ') {
+                Text.insert(i + 1, " ");
+            }
+
+            if (Text[i] == '.' && i + 2 < Text.length()) {
+                Text[i + 2] = toupper(Text[i + 2]);
+            }
+        }
+    }
+
+    std::cout << Text << endl;
+
+    return 0;
+}
